9ee27e2b4033321c0fee6abd0c788fa8c87344c4
Utkarsha-Padhye <133418624+Utkarsha-Padhye@users.noreply.github.com>
2023-06-01 17:03:15 +0530

    Add files via upload

---
+++ chalkapp.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk')
+
+console.log(chalk.green('Success'))
+console.log(chalk.green.bold('Success'))
+console.log(chalk.red.bold.inverse('Success'))
+//install chal ver 2.4.1
